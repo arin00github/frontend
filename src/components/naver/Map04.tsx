9187e2f03aacb5dfca5a20cd6570f791b1350682
이효정 <leeleejin325@gmail.com>
2022-03-08 16:47:26 +0900

    맵에다가 요소 추가하기

--- src/components/naver/Map04.tsx
+++ src/components/naver/Map04.tsx
@@ -11,19 +11,53 @@ export function Map04() {
   const dispatch = useMapDispatch();
 
   let mapObject;
-  let markerBox;
+  let contentEl: HTMLDivElement;
 
   function initMap() {
     mapObject = new naver.maps.Map("naver_map04", {
       zoom: 6,
       center: new naver.maps.LatLng(36.2253017, 127.6460516),
       zoomControl: true,
+      padding: { top: 100 },
       zoomControlOptions: {
         position: naver.maps.Position.TOP_LEFT,
         style: naver.maps.ZoomControlStyle.SMALL,
       },
     });
     dispatch({ type: "CHANGE_MAP", map: mapObject });
+  }
+
+  function initOption() {
+    contentEl = document.createElement("div");
+    contentEl.classList.add("iw_inner");
+    contentEl.style.width = "240px";
+    contentEl.style.height = "180px";
+    contentEl.style.position = "absolute";
+    contentEl.style.top = "0";
+    contentEl.style.right = "0";
+    contentEl.style.background = "#ffffff";
+    contentEl.style.zIndex = "100";
+    map.getElement().appendChild(contentEl);
+    console.log(map.getElement());
+
+    const titleEl = document.createElement("h3");
+    titleEl.innerText = "Map State";
+
+    const zoomEl = document.createElement("p");
+
+    const centerEl = document.createElement("p");
+
+    contentEl.appendChild(titleEl);
+    contentEl.appendChild(zoomEl);
+    contentEl.appendChild(centerEl);
+
+    naver.maps.Event.addListener(map, "zoom_changed", function (zoom) {
+      zoomEl.innerText = `zoom : ${zoom}`;
+    });
+
+    naver.maps.Event.addListener(map, "bounds_changed", function (bounds) {
+      centerEl.innerText = `bounds : ${bounds.toString()}`;
+    });
   }
 
   useEffect(() => {
@@ -33,13 +67,17 @@ export function Map04() {
 
   useEffect(() => {
     if (map !== null) {
-      //onLoadMarker();
+      initOption();
     }
   }, [map]);
 
   return (
     <>
-      <Box w="1200px" h="560px" id="naver_map04"></Box>
+      <Box w="1400px" h="680px" id="naver_map04" pos="relative">
+        {/* <Box w="240px" h="160px" pos="absolute" top={0} left={0}>
+          naver api
+        </Box> */}
+      </Box>
     </>
   );
 }
