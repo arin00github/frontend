82cf90078f71f973219cfb124f5fcd4cbc8ef46d
otter0104 <otter0104@innodep.com>
2022-09-16 10:44:50 +0900

    edit eslint, typescript config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,6 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
-  // testMatch: ["<rootDir>/__tests__/*.test.(js|jsx|ts|tsx)"],
+  testMatch: ["<rootDir>/__tests__/*.test.(js|jsx|ts|tsx)"],
 };
 export default config;
